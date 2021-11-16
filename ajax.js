8a3399ee0e901050fbcb85f8c6e19606e50f540f
remeezarasheed <37647001+remeezarasheed@users.noreply.github.com>
2021-11-16 19:11:30 +0530

    Changes in colours

--- ajax.js
+++ ajax.js
@@ -1,18 +1,31 @@
-
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-
-        if(this.readyState==4&&this.status==200){
-            var response = JSON.parse(this.responseText);
-            var Jshopping = response.shopping;
-            var tbl = document.getElementById('tbl');
-
-            for(i=0;i<Jshopping.length;i++){
-                
-                tbl.innerHTML += "<td>" +Jshopping[i].Serialno +"</td>" + "<td>" +Jshopping[i].Name +"</td>"+"<td>" +Jshopping[i].Quantity +"</td>"+"<td>" +Jshopping[i].Unit +"</td>" + "<td>" +Jshopping[i].Department +"</td>" + "<td>" +Jshopping[i].Notes +"</td>";
-            }
-      }
-
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var response = JSON.parse(this.responseText);
+    var Jshopping = response.shopping;
+    var tbl = document.getElementById("tbl");
+    for (i = 0; i < Jshopping.length; i++) {
+      tbl.innerHTML +=
+        "<td>" +
+        Jshopping[i].Serialno +
+        "</td>" +
+        "<td>" +
+        Jshopping[i].Name +
+        "</td>" +
+        "<td>" +
+        Jshopping[i].Quantity +
+        "</td>" +
+        "<td>" +
+        Jshopping[i].Unit +
+        "</td>" +
+        "<td>" +
+        Jshopping[i].Department +
+        "</td>" +
+        "<td>" +
+        Jshopping[i].Notes +
+        "</td>";
     }
-    xhttp.open("GET", "ajax.json", true);
-    xhttp.send();
+  }
+};
+xhttp.open("GET", "ajax.json", true);
+xhttp.send();
